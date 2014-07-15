139ae94ec85c27ebff00d3c2e7f33036cca1cc95
James Huang <james.huang@weiluen.com>
2014-07-14 19:22:08 -0500

    Testing out URL  import and try catch exception handling of MalformedURLException

--- app/src/main/java/com/weiluen/jameshuang/blogify/MainListActivity.java
+++ app/src/main/java/com/weiluen/jameshuang/blogify/MainListActivity.java
@@ -3,28 +3,40 @@ package com.weiluen.jameshuang.blogify;
 import android.app.ListActivity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames = {};
+    protected String[] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+        }
+        catch (MalformedURLException e ) {
+            Log.e(TAG, "Exception Caught: ", e);
+        }
 
         //Can't get the .getStringArray method from activity it is a subclass of resources so make a new resources object.
 
         Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
+        mBlogPostTitles = resources.getStringArray(R.array.android_names);
 
         //Create an ArrayAdapter to adapt an array
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
         //sets the adapter to the default list.
         setListAdapter(adapter);
 
